f530e99a6bdcf40a782f75c56a5d97b07042dc74
iamlydial <lydia.lavecchia@gmail.com>
2024-09-03 14:41:02 +0200

    Create BookedCarInformation.java

---
+++ src/com/jts/crs/BookedCarInformation.java
@@ -0,0 +1,12 @@
+package com.jts.crs;
+
+public class BookedCarInformation {
+    private Car car;
+    private Customer customer;
+
+    private int days;
+
+    public BookedCarInformation(Car car, Customer customer, int days){
+
+    }
+}
